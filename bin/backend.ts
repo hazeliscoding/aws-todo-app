c2cfd02087a7e897dd364e209381d61bcfb6abf8
hazeliscoding <duck_soop@outlook.com>
2024-04-27 23:12:19 -0500

    feat: 🎸 create and deploy schema for appsync

--- bin/backend.ts
+++ bin/backend.ts
@@ -4,6 +4,7 @@ import * as cdk from 'aws-cdk-lib';
 import { DatabaseStack } from '../lib/database-stack';
 import { ComputeStack } from '../lib/compute-stack';
 import { AuthStack } from '../lib/auth-stack';
+import { AppsyncStack } from '../lib/appsync-stack';
 
 // Determine the environment from a command-line argument or environment variable
 const environment = process.env.ENV || 'dev'; // Default to 'dev' if not specified
@@ -24,3 +25,11 @@ const authStack = new AuthStack(app, `TodoApp-${environment}-AuthStack`, {
   stage: environment,
   addUserPostConfirmation: computeStack.addUserToTableFunc,
 });
+const appsyncStack = new AppsyncStack(
+  app,
+  `TodoApp-${environment}-AppsyncStack`,
+  {
+    stage: environment,
+    userPool: authStack.todoUserPool,
+  }
+);
---
+++ lib/appsync-stack.ts
@@ -0,0 +1,51 @@
+import * as cdk from 'aws-cdk-lib';
+import * as awsAppSync from 'aws-cdk-lib/aws-appsync';
+import { UserPool } from 'aws-cdk-lib/aws-cognito';
+import { Construct } from 'constructs';
+import * as path from 'path';
+
+interface AppsyncStackProps extends cdk.StackProps {
+  stage: string;
+  userPool: UserPool;
+}
+
+export class AppsyncStack extends cdk.Stack {
+  public readonly api: awsAppSync.IGraphqlApi;
+
+  constructor(scope: Construct, id: string, props: AppsyncStackProps) {
+    super(scope, id, props);
+
+    this.api = this.createAppSyncApi(props);
+  }
+
+  createAppSyncApi(props: AppsyncStackProps) {
+    const api = new awsAppSync.GraphqlApi(this, 'TodoAppsyncApi', {
+      name: `TodoAppsyncApi-${props.stage}`,
+      definition: awsAppSync.Definition.fromFile(
+        path.join(__dirname, '../graphql/schema.graphql')
+      ),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: awsAppSync.AuthorizationType.USER_POOL,
+          userPoolConfig: {
+            userPool: props.userPool,
+          },
+        },
+        additionalAuthorizationModes: [
+          {
+            authorizationType: awsAppSync.AuthorizationType.IAM,
+          },
+        ],
+      },
+      logConfig: {
+        fieldLogLevel: awsAppSync.FieldLogLevel.ALL,
+      },
+    });
+
+    new cdk.CfnOutput(this, 'GraphQLAPIURL', {
+      value: api.graphqlUrl,
+    });
+
+    return api;
+  }
+}
